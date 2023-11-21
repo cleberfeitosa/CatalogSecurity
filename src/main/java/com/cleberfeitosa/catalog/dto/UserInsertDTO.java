d26a98345cef8f093927a80b63f40a50bd236b95
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2023-11-21 18:25:29 -0400

    Projeto Exemplo de Autorization e Autentication

---
+++ src/main/java/com/cleberfeitosa/catalog/dto/UserInsertDTO.java
@@ -0,0 +1,22 @@
+package com.cleberfeitosa.catalog.dto;
+
+import com.cleberfeitosa.catalog.services.validation.UserInsertValid;
+
+@UserInsertValid
+public class UserInsertDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+
+	private String password;
+
+	UserInsertDTO() {
+		super();
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/test/java/com/cleberfeitosa/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleberfeitosa.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class catalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
