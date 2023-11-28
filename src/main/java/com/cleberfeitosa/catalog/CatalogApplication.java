752f41dbbab240807bb9538af755622c46c6099f
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2023-11-28 19:31:24 -0400

    DockerFile e Jar criados

--- src/main/java/com/cleberfeitosa/catalog/CatalogApplication.java
+++ src/main/java/com/cleberfeitosa/catalog/CatalogApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatalogApplication {
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
